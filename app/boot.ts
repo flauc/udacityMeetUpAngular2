0065fbd54f34f4d71faf516d4a9eb5426453b290
flauc <filip.lauc93@gmail.com>
2016-03-19 21:11:02 +0100

    Setting up pages

--- app/boot.ts
+++
@@ -1,9 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {AppComponent} from "./app.component";
-import {NotificationsService} from "angular2-notifications/components";
-
-bootstrap(AppComponent, [
-    ROUTER_PROVIDERS,
-    NotificationsService
-]);
--- app/helpers/meet.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Meet {
-    name: string
-    type: string
-    host: string
-    start: Date
-    end: Date
-    location: string
-    message?: string
-}
